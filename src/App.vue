4232c2a6b94f2f7453549713021ec720db769326
dasmuli <dasmuli@web.de>
2021-03-05 16:58:30 +0100

    Initial working version of onsen.

--- src/App.vue
+++ src/App.vue
@@ -26,3 +26,17 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+<template id="main-page">
+  <v-ons-page>
+    <v-ons-toolbar>
+      <div class="center">Title</div>
+    </v-ons-toolbar>
+
+    <p style="text-align: center">
+      <v-ons-button @click="$ons.notification.alert('Hello World!')">
+        Click me!
+      </v-ons-button>
+    </p>
+  </v-ons-page>
+</template>
