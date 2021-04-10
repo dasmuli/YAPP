de2bd824ba558853d4ec7c98c832987f07749159
dasmuli <dasmuli@web.de>
2021-04-10 22:18:23 +0200

    Added finished tasks page.

--- src/pages/finishedpage.vue
+++ src/pages/finishedpage.vue
@@ -1,15 +1,91 @@
 
 <script>
-
-
 export default {
-  name: 'finishedpage',
-  key: 'finishedpage',
-}
+  name: "finishedpage",
+  key: "finishedpage",
+  computed: {
+    project() {
+      return this.$store.state.project.selected;
+    },
+  },
+  methods: {
+    backPage() {
+      this.$emit("pop-page");
+    },
+  },
+};
 </script>
 
-<template id="mainpage">
+<template id="finishedpage">
   <v-ons-page>
-   Hallo
+    <v-ons-toolbar>
+      <div class="left">
+        <v-ons-back-button>Back</v-ons-back-button>
+      </div>
+      <div class="center">Finished: {{ project.name }}</div>
+    </v-ons-toolbar>
+
+    <div class="content">
+      <v-ons-list>
+        <v-ons-list-header>Finished Tasks</v-ons-list-header>
+        <v-ons-list-item
+          expandable
+          v-for="task in project.closedTasks"
+          :key="task.id"
+        >
+          <label class="center" v-if="task.isMilestone" style="color: #0076ff">
+            {{ task.name }}
+          </label>
+          <label class="center" v-else>
+            {{ task.name }}
+          </label>
+          <div class="expandable-content">
+            <v-ons-list>
+              <v-ons-list-item>
+                <label class="left">
+                  <v-ons-icon icon="md-edit"></v-ons-icon>
+                </label>
+                <label class="center">
+                  {{ task.name }}
+                </label>
+              </v-ons-list-item>
+              <v-ons-list-item>
+                <label class="left">
+                  <v-ons-icon icon="md-bike"></v-ons-icon>
+                </label>
+                {{ task.effort }}
+              </v-ons-list-item>
+              <v-ons-list-item>
+                <label class="left">
+                  <v-ons-checkbox
+                    :input-id="'checkbox-' + task.id"
+                    value="true"
+                    v-model="task.isMilestone"
+                    disabled="true"
+                  >
+                  </v-ons-checkbox>
+                </label>
+                <label class="center"> Milestone </label>
+              </v-ons-list-item>
+              <v-ons-list-item>
+                <label class="left">
+                  <v-ons-icon icon="md-info"></v-ons-icon>
+                </label>
+                {{ task.description }}
+              </v-ons-list-item>
+              <v-ons-list-item>
+                <v-ons-button
+                  icon="md-assignment-check"
+                  @click="
+                    $ons.notification.toast('Reopened', { timeout: 1500 });
+                    closeTask(task);
+                  "
+                ></v-ons-button>
+              </v-ons-list-item>
+            </v-ons-list>
+          </div>
+        </v-ons-list-item>
+      </v-ons-list>
+    </div>
   </v-ons-page>
 </template>
